51ea6e1a97c14774c3e8a81fc262c717b8188efe
Jasser Cedeño <jassergerardoc@gmail.com>
2024-08-03 14:09:51 -0500

    Actualizacion del app.js

--- src/js/app.js
+++ src/js/app.js
@@ -21,6 +21,7 @@ function navegacionFija() {
 function crearGaleria() {
     const CANTIDAD_IMAGENES = 16
     const galeria = document.querySelector('.galeria-imagenes')
+    
     for(let i = 1; i <= CANTIDAD_IMAGENES; i++) {
         const imagen = document.createElement('PICTURE')
         imagen.innerHTML = `
